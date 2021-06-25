7ec69d8df24df2838ff5dd2cac3872fd2f1df493
tyty979812 <tyty979812@gmail.com>
2021-06-26 03:25:38 +0800

    Add electron-reload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron.cmd') // windows用electron.cmd
+});
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
