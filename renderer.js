65c3ee75e09e97d47e585d51a56b2e9f4c29714f
tyty979812 <tyty979812@gmail.com>
2021-07-26 03:04:58 +0800

    VS code(1)

--- renderer.js
+++ renderer.js
@@ -1,1 +1,11 @@
-console.log(123);
+document.querySelectorAll(".iconBtn").forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        const leftMenu = document.querySelector("#left-menu");
+        if (leftMenu.classList.contains('closed')){
+            leftMenu.classList.remove('closed');
+        }
+        else{
+            leftMenu.classList.add('closed');
+        }
+    })
+})
